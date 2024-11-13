ea2e38f5a30046b72389a201dca4ad0f66b782a4
nbvhung <nguyenbaviethung300805@gmail.com>
2024-11-13 16:15:30 +0700

    Create CPP0629 - DANH SÁCH DOANH NGHIỆP NHẬN SINH VIÊN THỰC TẬP - 2.cpp

---
+++ CPP0629 - DANH SÁCH DOANH NGHIỆP NHẬN SINH VIÊN THỰC TẬP - 2.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class sinhvien{
+    private:
+        string ma, ten;
+        int sl;
+    public:
+        friend istream& operator >> (istream&, sinhvien&);
+        friend ostream& operator << (ostream&, sinhvien);
+
+        int getsl(){
+            return this->sl;
+        }
+
+        string getma(){
+            return this->ma;
+        }
+        
+};
+
+istream& operator >> (istream& in, sinhvien &a){
+    in.ignore();
+    getline(in, a.ma);
+    getline(in, a.ten);
+    cin>>a.sl;
+    return in;
+}
+
+ostream& operator << (ostream& out, sinhvien a){
+    out<<a.ma<<" "<<a.ten<<" "<<a.sl<<endl;
+    return out;
+}
+
+bool cmp(sinhvien a, sinhvien b){
+    if(a.getsl() == b.getsl()) return a.getma() < b.getma();
+    return a.getsl() > b.getsl();
+}
+
+int main(){
+    int n; cin>>n;
+    sinhvien a[n];
+    for(int i=0; i<n; i++) cin>>a[i];
+    sort(a,a+n,cmp);
+    int q; cin>>q;
+    cin.ignore();
+    while(q--){
+        int x,y; cin>>x>>y;
+        cout<<"DANH SACH DOANH NGHIEP NHAN TU "<<x<<" DEN "<<y<<" SINH VIEN:\n";
+        for(int i=0; i<n; i++){
+            if(a[i].getsl() >= x && a[i].getsl() <=y){
+                cout<<a[i];
+            }
+        }
+    }
+}
